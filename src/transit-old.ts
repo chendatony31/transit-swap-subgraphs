3727c1242ed8b0d70984cb1f65af3a894c7eb8a6
chendatony31 <31647753@qq.com>
2022-08-05 22:27:42 +0800

    arb old abi

--- src/transit-old.ts
+++ src/transit-old.ts
@@ -1,21 +1,17 @@
 import {
-  SwapChannel
+  Swap
 } from "../generated/TransitOld/TransitOld"
 import { Order } from "../generated/schema"
 
-export function handleSwapChannel(event: SwapChannel): void {
+export function handleSwap(event: Swap): void {
   let entity = new Order(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
   entity.blockNumber = event.block.number
   entity.from = event.transaction.from
-  entity.trader = event.params.trader
   entity.token0 = event.params.token0
   entity.token1 = event.params.token1
-  entity.channel = event.params.channel
-  entity.amount = event.params.amount
-  entity.fees = event.params.fees
-  entity.toChainID = event.params.toChainID
-  entity.isOld = true
+  entity.amountIn = event.params.amountIn
+
   entity.save()
 }
