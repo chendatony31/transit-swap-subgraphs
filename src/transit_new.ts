2c3d2298e6b001d5d64366924b95d6b5c62d19d8
chendatony31 <31647753@qq.com>
2022-10-31 22:18:11 +0800

    add v4

---
+++ src/transit_new.ts
@@ -0,0 +1,34 @@
+import {
+    TransitSwapped
+} from "../generated/TransitNew/TransitNew"
+import { Order, User } from "../generated/schema"
+
+export function handleSwap(event: TransitSwapped): void {
+    let newEntity = new Order(
+        event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    )
+    newEntity.blockNumber = event.block.number
+    newEntity.hash = event.transaction.hash
+    newEntity.from = event.transaction.from
+    newEntity.to = event.transaction.to
+    newEntity.token0 = event.params.srcToken
+    newEntity.token1 = event.params.dstToken
+    newEntity.channel = event.params.channel
+    newEntity.amountIn = event.params.amount
+    newEntity.toChainID = event.params.toChainID
+    newEntity.timestamp = event.block.timestamp
+    newEntity.gasLimit = event.transaction.gasLimit
+    newEntity.nonce = event.transaction.nonce
+    newEntity.save()
+
+    let address = User.load(event.transaction.from.toHexString());
+
+    if (address == null) {
+        address = new User(event.transaction.from.toHexString())
+        address.count = 1
+    }
+    else {
+        address.count += 1
+    }
+    address.save()
+}
