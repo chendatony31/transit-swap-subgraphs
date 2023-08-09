dbff431fcef0622ed7efc06caaaee0971292e36f
chendatony31 <31647753@qq.com>
2023-08-09 20:38:19 +0800

    add polygon v5

---
+++ src/transit_v5.ts
@@ -0,0 +1,37 @@
+import {
+    TransitSwapped
+} from "../generated/TransitV5/TransitV5"
+import { Order, User } from "../generated/schema"
+
+export function handleSwap(event: TransitSwapped): void {
+    let newEntity = new Order(
+        event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    )
+    newEntity.blockNumber = event.block.number
+    newEntity.hash = event.transaction.hash
+    newEntity.from = event.transaction.from
+    newEntity.to = event.transaction.to
+    newEntity.trader = event.transaction.from
+    newEntity.receiver = event.params.dstReceiver
+    newEntity.token0 = event.params.srcToken
+    newEntity.token1 = event.params.dstToken
+    newEntity.channel = event.params.channel
+    newEntity.amountIn = event.params.amount
+    newEntity.amountOut = event.params.returnAmount
+    newEntity.toChainID = event.params.toChainID
+    newEntity.timestamp = event.block.timestamp
+    newEntity.gasLimit = event.transaction.gasLimit
+    newEntity.nonce = event.transaction.nonce
+    newEntity.save()
+
+    let address = User.load(event.transaction.from.toHexString());
+
+    if (address == null) {
+        address = new User(event.transaction.from.toHexString())
+        address.count = 1
+    }
+    else {
+        address.count += 1
+    }
+    address.save()
+}
